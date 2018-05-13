4dafbd48b18ebb4f2be3d4a2156ed4c1c2a327a2
Sigurður Kári Árnason <siggispes@Sigurdurs-MacBook-Pro.local>
2018-05-13 17:26:35 +0000

    Part of icelandic translation

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   tokenKey: process.env.BIGFIVE_SAVE_TOKEN_KEY || 'Gibberish, jibberish, jibber-jabber and gobbledygook',
-  dbConnection: process.env.DB_CONNECTION || 'mongodb://localhost/bigfive',
+  dbConnection: process.env.DB_CONNECTION || 'mongodb://bigfive:bigfive@ds155577.mlab.com:55577/bigfive',
   dbCollection: process.env.DB_COLLECTION || 'results'
 }
